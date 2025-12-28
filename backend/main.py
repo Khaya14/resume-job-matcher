4b872d0f00a757376b87e12aad4d474f9a669d63
Khaya14 <77794172+Khaya14@users.noreply.github.com>
2025-12-28 14:33:32 +0200

    Update Gemini Model to geminin-2.5-flash to fix error 404

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-3-flash')  # FIXED: Correct model name
+model = genai.GenerativeModel('gemini-2.5-flash')  # FIXED: Correct model name
 
 
 app = FastAPI(title="BitTrio - Resume Job Matcher")
